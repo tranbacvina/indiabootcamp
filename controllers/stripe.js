d341aa7981598d786442d57d72b89f8fbc7d9d66
Henry <tranbacvina@gmail.com>
2023-09-05 08:37:52 +0700

    add live key stripe

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_LIVE_KEY);
 const endpointSecret = "whsec_8017e25955ec281120d71e7df71fb710b746d70c03217759ad65a2cf4cc3c0b5";
 const db = require("../models");
 const botTelegram = require("../service/telegram_noti");
