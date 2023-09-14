f700bd0a9acff54e60cc3c705a8767a031e29093
Henry <tranbacvina@gmail.com>
2023-09-14 08:49:40 +0700

    add vietcombank admin

--- routers/admin/index.js
+++ routers/admin/index.js
@@ -2,10 +2,13 @@ const Routers = require("express").Router();
 const course = require("./course")
 const order = require("./order")
 const orderitem = require("./orderitem")
+const vietcombank = require("../../controllers/vietcombank")
+
 
 Routers.use("/course", course)
 Routers.use("/order", order)
 Routers.use("/orderitem", orderitem)
+Routers.get("/vietcombank", vietcombank.vietcombankApi)
 
 
 
